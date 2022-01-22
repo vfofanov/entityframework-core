ad3bfe8cb13c8c032c9694d2ee8f81f4afc3d002
Vasiliy Fofanov <vasiliy.fofanov@stenn.com>
2022-01-22 16:34:42 +0300

    Add in memory package

---
+++ src/Stenn.EntityFrameworkCore.SqlServer.Tests/MigrationsTest.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Stenn.EntityFrameworkCore.DbContext.Initial;
+
+namespace Stenn.EntityFrameworkCore.SqlServer.Tests
+{
+    public class MigrationsTest
+    {
+        protected InitialDbContext InitialDbContext;
+        protected MainDbContext MainDbContext;
+        protected IServiceProvider ServiceProvider;
+
+        protected static string GetConnectionString(string dbName)
+        {
+            return $@"Data Source=.\SQLEXPRESS;Initial Catalog={dbName};Integrated Security=SSPI";
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+
+            var dbName = "stenn_efcore_tests";
+            var connectionString = GetConnectionString(dbName);
+
+            void DbContextConfigure(DbContextOptionsBuilder builder)
+            {
+                //builder.ReplaceService<>()
+                builder.UseSqlServer(connectionString);
+                //builder.UseInMemoryDatabase(dbName);
+            }
+
+            services.AddDbContext<InitialDbContext>(DbContextConfigure);
+            services.AddDbContext<MainDbContext>(DbContextConfigure);
+
+            ServiceProvider = services.BuildServiceProvider();
+            InitialDbContext = ServiceProvider.GetRequiredService<InitialDbContext>();
+            MainDbContext = ServiceProvider.GetRequiredService<MainDbContext>();
+        }
+
+        [Test]
+        public async Task InitialMigration()
+        {
+            await RunMigrations(InitialDbContext);
+            Assert.Pass();
+        }
+        [Test]
+        public async Task MainMigration()
+        {
+            await RunMigrations(MainDbContext);
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task InitialAndMainMigration()
+        {
+            await RunMigrations(InitialDbContext, false);
+            await RunMigrations(MainDbContext);
+            Assert.Pass();
+        }
+
+        private static async Task RunMigrations(Microsoft.EntityFrameworkCore.DbContext dbContext, bool deleteDb = true)
+        {
+            var database = dbContext.Database;
+            try
+            {
+                await database.MigrateAsync();
+            }
+            finally
+            {
+                if (deleteDb)
+                {
+                    await database.EnsureDeletedAsync();
+                }
+            }
+        }
+
+        
+    }
+}
